fcfc1a9423b50e034a1dc3e224f5d6e644a52c6d
nerjib <kabirnajib0@gmail.com>
2020-04-19 07:28:29 +0000

    Updates

---
+++ precache-manifest.5d6164edfac67877597ba8f78a9dcad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0f1ab5330c83c3662e75dfa509dea9",
+    "url": "/kadtrack/index.html"
+  },
+  {
+    "revision": "4ce17aa1af244d03f197",
+    "url": "/kadtrack/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "863c1ff899ca15a93052",
+    "url": "/kadtrack/static/css/main.fce866dd.chunk.css"
+  },
+  {
+    "revision": "4ce17aa1af244d03f197",
+    "url": "/kadtrack/static/js/2.7ae7440e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kadtrack/static/js/2.7ae7440e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863c1ff899ca15a93052",
+    "url": "/kadtrack/static/js/main.8be334af.chunk.js"
+  },
+  {
+    "revision": "3a8b9c20836556a0d785",
+    "url": "/kadtrack/static/js/runtime-main.7fd7219f.js"
+  }
+]);
